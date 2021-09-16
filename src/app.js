ee4598f0b2eb6e3bd48f71f86ff8389bddf5d178
Amr <Elshehaly>
2021-09-16 11:44:47 +0200

    using express use for cors

--- src/app.js
+++ src/app.js
@@ -27,7 +27,25 @@ const googleRouter = require('./auth/google-auth')
 // }
 
 const app = express()
-app.use(cors({origin: 'https://upbeat-bohr-db3c71.netlify.app'}))
+// app.use(cors({origin: 'https://upbeat-bohr-db3c71.netlify.app'}))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://upbeat-bohr-db3c71.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.static(publicDirectoryPath))
 
 const server = http.createServer(app)
